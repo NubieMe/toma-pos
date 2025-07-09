776deff9fe608ea76c4986c5e2c7913faec73976
fahmi <fahmi.mellow@gmail.com>
2025-07-09 10:25:41 +0700

    chore: adjust role route

--- src/app/api/role/[id]/route.ts
+++ src/app/api/role/[id]/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ResponseError } from "@/lib/error/response-error";
 import { errorHandler } from "@/utils/helper";
-import { deleteRole, getRoleById, updateRole } from "@/repositories/role.server";
+import { ServiceFactory } from "@/services/service-factory";
+import { Session } from "@/types/session";
 
 const unedited = ["root", "administrator"];
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     try {
         const { id } = await params;
-        const data = await getRoleById(id);
+        const data = await ServiceFactory.getOne('role', id);
 
         return NextResponse.json({ data });
     } catch (error) {
@@ -20,14 +21,15 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
     try {
         const { id } = await params;
         const body = await req.json();
-        const existingRole = await getRoleById(id);
+        const existingRole = await ServiceFactory.getOne('role', id);
+        const user = JSON.parse(req.headers.get('x-user-payload')!) as Session
 
         if (!existingRole) {
             return NextResponse.json({ message: "Role not found" }, { status: 404 });
         } else if (unedited.includes(existingRole.name)) {
             return NextResponse.json({ message: "Role cannot be edited" }, { status: 403 });
         }
-        const data = await updateRole(id, body);
+        const data = await ServiceFactory.update('role', id, body, user.id);
 
         return NextResponse.json({ message: "Role updated successfully", data });
     } catch (error) {
@@ -38,14 +40,15 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
 export async function DELETE(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     try {
         const { id } = await params;
-        const existingRole = await getRoleById(id);
+        const existingRole = await ServiceFactory.getOne('role', id);
+        const user = JSON.parse(req.headers.get('x-user-payload')!) as Session
 
         if (!existingRole) {
             return NextResponse.json({ message: "Role not found" }, { status: 404 });
         } else if (unedited.includes(existingRole.name)) {
             return NextResponse.json({ message: "Role cannot be deleted" }, { status: 403 });
         }
-        const data = await deleteRole(id);
+        const data = await ServiceFactory.delete('role', id, user.id);
 
         return NextResponse.json({ message: "Role deleted successfully", data });
     } catch (error) {
--- src/app/api/role/route.ts
+++ src/app/api/role/route.ts
@@ -2,47 +2,69 @@ import { NextRequest, NextResponse } from "next/server";
 import { errorHandler } from "@/utils/helper";
 import { ResponseError } from "@/lib/error/response-error";
 import { countRole, getAllRoles, getRoleByName, insertRole } from "@/repositories/role.server";
-import { getUserSessionFromRequest } from "@/lib/session";
+import { Session } from "@/types/session";
 import { Prisma } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
-    try {
-        const body = await req.json();
+  try {
+    const body = await req.json();
 
-        const existingRole = await getRoleByName(body.name);
+    const existingRole = await getRoleByName(body.name);
 
-        if (existingRole) {
-            return NextResponse.json({
-                message: "Role already exists" 
-            }, { status: 400 });
-        }
-
-        const data = await insertRole(body);
-
-        return NextResponse.json({
-            message: "Role created successfully",
-            data,
-        }, { status: 201 });
-    } catch (error) {
-        return errorHandler(error as ResponseError);
+    if (existingRole) {
+      return NextResponse.json({
+        message: "Role already exists" 
+      }, { status: 400 });
     }
+
+    const data = await insertRole(body);
+
+    return NextResponse.json({
+      message: "Role created successfully",
+      data,
+    }, { status: 201 });
+  } catch (error) {
+    return errorHandler(error as ResponseError);
+  }
 }
 
 export async function GET(req: NextRequest) {
-    try {
-        const user = getUserSessionFromRequest(req);
+  try {
+    const user = JSON.parse(req.headers.get('x-user-payload')!) as Session
+    const query = req.nextUrl.searchParams
+    const page = parseInt(query.get('page')!) || 1
+    const limit = parseInt(query.get('limit')!) || 10
+    const skip = (page - 1) * limit
+    const search = query.get('search') ? JSON.parse(query.get('search')!) : {}
 
-        const where: Prisma.RoleWhereInput = user?.role.name === 'root' ? {} : { name: { not: 'root' } };
-        const [data, total] = await Promise.all([
-            await getAllRoles(where),
-            await countRole(where as Prisma.RoleCountArgs),
-        ])
-        
-        return NextResponse.json({
-            data,
-            total
-        });
-    } catch (error) {
-        return errorHandler(error as ResponseError);
+    let order: Prisma.RoleOrderByWithRelationInput = { created_date: 'desc' }
+    if (query.get('order')) {
+      const sort = query.get('order')!.split('-')
+      order = { [sort[0]]: sort[1] }
     }
+
+    const where: Prisma.RoleWhereInput = user?.role.name === 'root' ? {} : { name: { not: 'root' } };
+
+    Object.keys(search).forEach(key => {
+      if (key.includes('-')) {
+        console.log(key, search[key])
+        const fields = key.split('-')
+        where.OR = fields.map(f => ({ [f]: { contains: search[key] } }))
+      } else if (key === 'name' || key === 'description') {
+        where[key] = { contains: search[key] }
+      }
+    })
+
+    const [data, total] = await Promise.all([
+      getAllRoles(where, limit, skip, order),
+      countRole(where),
+    ])
+
+    return NextResponse.json({
+      data,
+      total
+    });
+  } catch (error) {
+    return errorHandler(error as ResponseError);
+  }
 }
