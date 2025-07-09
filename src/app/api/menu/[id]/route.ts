d2c5ad054f07196718b4eb75db298a821cc286bf
fahmi <fahmi.mellow@gmail.com>
2025-07-09 10:25:02 +0700

    chore: adjust menu route

--- src/app/api/menu/[id]/route.ts
+++ src/app/api/menu/[id]/route.ts
@@ -2,13 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { errorHandler } from "@/utils/helper";
 import { ResponseError } from "@/lib/error/response-error";
 import { ServiceFactory } from "@/services/service-factory";
+import { Session } from "@/types/session";
 
 export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     try {
         const { id } = await params
         const body = await req.json()
+        const user = JSON.parse(req.headers.get('x-user-payload')!) as Session
 
-        const data = await ServiceFactory.update('menu', id, body)
+        const data = await ServiceFactory.update('menu', id, body, user.id)
 
         return NextResponse.json({ message: "Menu updated successfully", data })
     } catch (error) {
@@ -16,10 +18,11 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
     }
 }
 
-export async function DELETE({ params }: { params: Promise<{ id: string }> }) {
+export async function DELETE(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     try {
         const { id } = await params
-        const data = await ServiceFactory.delete('menu', id)
+        const user = JSON.parse(req.headers.get('x-user-payload')!) as Session
+        const data = await ServiceFactory.delete('menu', id, user!.id)
 
         return NextResponse.json({ message: "Menu deleted successfully", data })
     } catch (error) {
--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -1,93 +1,123 @@
 import { errorHandler } from "@/utils/helper"
 import { ResponseError } from "@/lib/error/response-error"
 import { NextRequest, NextResponse } from "next/server"
-import { getAllMenu, getSidebarMenu, insertMenu } from "@/repositories/menu.server"
+import { getSidebarMenu } from "@/repositories/menu.server"
 import { prisma } from "@/lib/prisma"
 import { serialize } from "cookie"
-import { getUserSessionFromRequest, isProd } from "@/lib/session"
+import { Session } from "@/types/session"
 import { Menu } from "@/types/menu"
+import config from "@/config"
+import { ServiceFactory } from "@/services/service-factory"
+import { Prisma } from "@prisma/client"
 
 export async function GET(req: NextRequest) {
-    try {
-        const params = req.nextUrl.searchParams
-        const sidebar = params.get('sidebar') === 'true'
-        const user = getUserSessionFromRequest(req)
-
-        let data = (sidebar ? await getSidebarMenu() : await getAllMenu()) as Menu[]
-
-        const availMenu: string[] = [];
-        if (sidebar) {
-            data = data.filter((menu: Menu) => {
-                if (menu.permissions?.some(p => p.role_id === user?.role.id)) {
-
-                    const subMenu = menu.children?.filter((subMenu: Menu) => {
-                        if (subMenu.permissions?.some(p => p.role_id === user?.role.id)) {
-                            
-                            const grandSubMenu = subMenu.children?.filter((grandSubMenu: Menu) => {  
-                                if (grandSubMenu.permissions?.some(p => p.role_id === user?.role.id)) {
-
-                                    if (grandSubMenu.path) availMenu.push(grandSubMenu.path)
-                                    return grandSubMenu
-                                }
-                            })
-
-                            if (subMenu.path) availMenu.push(subMenu.path)
-                            return grandSubMenu
-                        }
-                    })
-                    
-                    if (menu.path) availMenu.push(menu.path)
-                    return subMenu
-                }
-            })
-        }
-
-        const res = NextResponse.json({ data })
-        if (sidebar) res.headers.set(
-            'Set-Cookie', 
-            serialize('sidecook', 
-                JSON.stringify(availMenu),
-                {
-                    path: '/',
-                    secure: isProd,
-                    sameSite: 'lax',
-                    httpOnly: true,
-                }
-            ))
-        return res
-    } catch (error) {
-        return errorHandler(error as ResponseError)
+  try {
+    const params = req.nextUrl.searchParams
+    const sidebar = params.get('sidebar') === 'true'
+    const user = JSON.parse(req.headers.get('x-user-payload')!) as Session
+  
+    const include: Prisma.MenuInclude = {
+      parent: true,
     }
+
+    // eslint-disable-next-line prefer-const
+    let { data, total } = (sidebar ? await getSidebarMenu() : await ServiceFactory.getAll('menu', params, include))
+
+    const availMenu: string[] = [];
+    if (sidebar) {
+      data = data.filter((menu: Menu) => {
+        if (menu.permissions?.some(p => p.role_id === user?.role.id)) {
+          const subMenu = menu.children?.filter((subMenu: Menu) => {
+            if (subMenu.permissions?.some(p => p.role_id === user?.role.id)) {
+                
+              const grandSubMenu = subMenu.children?.filter((grandSubMenu: Menu) => {  
+                if (grandSubMenu.permissions?.some(p => p.role_id === user?.role.id)) {
+
+                  if (grandSubMenu.path) availMenu.push(grandSubMenu.path)
+                  return grandSubMenu
+                }
+              })
+
+              if (subMenu.path) availMenu.push(subMenu.path)
+              return grandSubMenu
+            }
+          })
+          
+          if (menu.path) availMenu.push(menu.path)
+          return subMenu
+        }
+      })
+    }
+
+    const res = NextResponse.json({ data, total })
+    if (sidebar) res.headers.set(
+      'Set-Cookie', 
+      serialize('sidecook', 
+        JSON.stringify(availMenu),
+        {
+          path: '/',
+          secure: config.production,
+          sameSite: 'lax',
+          httpOnly: true,
+        }
+      ))
+    return res
+  } catch (error) {
+    return errorHandler(error as ResponseError)
+  }
 }
 
 export async function POST(req: NextRequest) {
-    try {
-        const body = await req.json()
-        const data = await insertMenu(body)
+  try {
+    const body = await req.json()
+    const user = JSON.parse(req.headers.get('x-user-payload')!) as Session
+    const data = await ServiceFactory.create('menu', body, user.id)
 
-        const role = await prisma.role.findUnique({ where: { name: 'root' } })
+    const role = await prisma.role.findUnique({ where: { name: 'root' } })
+    const roleAdmin = await prisma.role.findUnique({ where: { name: 'administrator' } })
 
-        if (role) {
-            const permission = await prisma.permission.create({
-                data: {
-                    role_id: role.id!,
-                    menu_id: data.id,
-                    create: true,
-                    read: true,
-                    update: true,
-                    delete: true,
-                },
-                include: {
-                    role: true,
-                    menu: true
-                }
-            })
-
-            data.permissions = [permission]
+    if (role) {
+      const permission = await prisma.permission.create({
+        data: {
+          role_id: role.id!,
+          menu_id: data.id,
+          create: true,
+          read: true,
+          update: true,
+          delete: true,
+          created_by: user.id
+        },
+        include: {
+          role: true,
+          menu: true
         }
+      })
 
-        return NextResponse.json({ message: "Menu created successfully", data })
-    } catch (error) {
-       return errorHandler(error as ResponseError) 
+      data.permissions = [permission]
     }
+
+    if (roleAdmin) {
+      const permission = await prisma.permission.create({
+        data: {
+          role_id: roleAdmin.id!,
+          menu_id: data.id,
+          create: true,
+          read: true,
+          update: true,
+          delete: true,
+          created_by: user.id
+        },
+        include: {
+          role: true,
+          menu: true
+        }
+      })
+
+      data.permissions = data.permissions ? [...data.permissions, permission] : [permission]
+    }
+
+    return NextResponse.json({ message: "Menu created successfully", data })
+  } catch (error) {
+    return errorHandler(error as ResponseError)
+  }
 }
