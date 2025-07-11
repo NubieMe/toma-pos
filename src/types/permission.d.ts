299baace93c1471c0bd4f0053ec9c0caf9b0254b
fahmi <fahmi.mellow@gmail.com>
2025-07-11 22:19:53 +0700

    chore: add some types for permissions

--- src/types/permission.d.ts
+++ src/types/permission.d.ts
@@ -2,3 +2,22 @@ export type PermissionID = {
   role_id: string
   menu_id: string
 }
+
+export type PermissionObject = {
+  access: boolean;
+  read: boolean;
+  create: boolean;
+  update: boolean;
+  delete: boolean;
+};
+
+export type PermissionsState = Record<string, Record<string, PermissionObject>>;
+
+export type RawPermissionData = {
+  role_id: string;
+  menu_id: string;
+  read: boolean;
+  create: boolean;
+  update: boolean;
+  delete: boolean;
+};
