a694e6d564e2f8a9d381fe6c21db8257c6e782f8
fahmi <fahmi.mellow@gmail.com>
2025-07-18 15:09:01 +0700

    chore: convert back separator before return

--- src/repositories/company.server.ts
+++ src/repositories/company.server.ts
@@ -1,5 +1,20 @@
 import { prisma } from "@/lib/prisma"
+import { Company } from "@/types/company";
+import { convertSeparatorBack } from "@/utils/helper";
+import { Separator } from "@prisma/client";
 
-export const getCompany = async () => {
-    return await prisma.company.findFirst();
+export async function getCompany(): Promise<Company | null> {
+    const data = await prisma.company.findFirst();
+
+    if (!data) return null;
+
+    (Object.keys(data) as Array<keyof typeof data>).forEach(key => {
+        if (key.includes('separator')) {
+            const value = (data[key] as Separator | null)
+            // @ts-expect-error if you know Typescript you know what I'm doing
+            data[key] = convertSeparatorBack(value)
+        }
+    })
+
+    return (data as Company)
 }
