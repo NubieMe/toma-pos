db76330fcb8bcfbf68f0ea78ebfc59cb8c97bd6d
fahmi <fahmi.mellow@gmail.com>
2025-07-18 14:35:22 +0700

    chore: adjust getcompany repo

--- src/types/company.d.ts
+++ src/types/company.d.ts
@@ -1,8 +1,23 @@
+import { Separator } from "@/constant/enum";
+
 export type Company = {
     id: string;
     name: string;
-    address?: string;
-    phone?: string;
-    email?: string;
-    logo?: string;
+    address: string | null;
+    phone: string | null;
+    email: string | null;
+    logo: string | null;
+    category_auto: boolean;
+    category_format: string | null;
+    category_separator: Separator;
+    item_auto: boolean;
+    item_format: string | null;
+    item_separator: Separator;
+    user_auto: boolean;
+    user_format: string | null;
+    user_separator: Separator;
+    created_date: Date;
+    updated_date: Date;
+    created_by: string | null;
+    updated_by: string | null;
 };
