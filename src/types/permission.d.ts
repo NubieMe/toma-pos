631670b85e482ed71873d89e6d04000d951c29e6
fahmi <fahmi.mellow@gmail.com>
2025-08-01 14:24:09 +0700

    chore: change types according changes in db

--- src/types/permission.d.ts
+++ src/types/permission.d.ts
@@ -1,23 +1,24 @@
+import { ActionTable } from "./action";
+
 export type PermissionID = {
   role_id: string
   menu_id: string
 }
 
-export type PermissionObject = {
-  access: boolean;
-  read: boolean;
-  create: boolean;
-  update: boolean;
-  delete: boolean;
-};
+export type Permission = {
+  created_date: Date;
+  updated_date: Date | null;
+  created_by: string | null;
+  updated_by: string | null;
+  role_id: string;
+  menu_id: string;
+  actions: ActionTable[];
+}
 
-export type PermissionsState = Record<string, Record<string, PermissionObject>>;
+export type PermissionsState = Record<string, Record<string, string[]>>;
 
 export type RawPermissionData = {
   role_id: string;
   menu_id: string;
-  read: boolean;
-  create: boolean;
-  update: boolean;
-  delete: boolean;
+  actions: string[];
 };
