69723183cd4aa16c4dea3470dc4c071ba7227fed
fahmi <fahmi.mellow@gmail.com>
2025-08-14 13:43:58 +0700

    feat: add code field to Branch model and update related components

--- src/app/(session)/branch/schema.ts
+++ src/app/(session)/branch/schema.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const branchSchema = z.object({
+  code: z.string().nonempty("Singkatan tidak boleh kosong").min(3, "Singkatan minimal 3 karakter").max(4, "Singkatan maksimal 4 karakter"),
   name: z.string().nonempty("Name tidak boleh kosong"),
   address: z.string().optional(),
   phone: z.string().optional(),
